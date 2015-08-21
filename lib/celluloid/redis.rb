c45f02a6d766858a340bdec1330a487159349359
Vladimir Kochnev <hashtable@yandex.ru>
2015-08-21 16:29:42 +0300

    Synchronize redis operations with task-aware latch.

--- lib/celluloid/redis.rb
+++ lib/celluloid/redis.rb
@@ -1,3 +1,25 @@
 require "redis"
 
 require "celluloid/redis/version"
+require "celluloid/io"
+
+module Celluloid
+  class Redis < ::Redis
+    VERSION = CelluloidRedis::VERSION
+
+    def initialize(*args)
+      super
+      if @options[:driver] == :celluloid
+        @latch = Celluloid::IO::Stream::Latch.new
+      end
+    end
+
+    def synchronize
+      if @latch
+        @latch.synchronize { yield @client }
+      else
+        super(&proc)
+      end
+    end
+  end
+end
